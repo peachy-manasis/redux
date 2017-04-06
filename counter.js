c52b41b78f61ce624b4c57c796dbcb4270ca8379
peachy-manasis <peachy.manasis@crmworks.asia>
2017-04-06 16:40:33 +0800

    refactor-best-practices
    - refactor code to use best practices

--- counter.js
+++ counter.js
@@ -1,60 +1,61 @@
-// reducer
-function counter(currentState, action)
+//reducer
+function counter(state, action)
 {
+    if(typeof  state === 'undefined')
+    {
+        return { count: 0 };
+    }
     var nextState = {
-        count: currentState.count
+        count: state.count
     };
-
-    switch (action.type) {
+    switch (action.type)
+    {
         case 'ADD':
-            nextState.count = currentState.count + 1;
+            nextState.count = state.count + 1;
             return nextState;
             break;
+
         case 'MINUS':
-            nextState.count = currentState.count - 1;
+            nextState.count = state.count - 1;
             return nextState;
             break;
+
         case 'RESET':
             nextState.count = 0;
             return nextState;
             break;
+
         default:
-            return currentState;
+            return state;
     }
 }
 
-// state
-var state = {
-    count: 0
-};
-
-// store
-var store = Redux.createStore(counter, state);
+//store
+var store = Redux.createStore(counter);
 var counterEl = document.getElementById('counter');
 
-function render()
-{
+function render() {
     var state = store.getState();
     counterEl.innerHTML = state.count.toString();
 }
 
+render();
 store.subscribe(render);
 
-// actions
+// ACTIONS
+// Best Practice: capitalize actions
 document.getElementById('add')
     .addEventListener('click', function()
     {
-        store.dispatch({type: 'ADD'})
+        store.dispatch({ type: 'ADD' })
     });
-
 document.getElementById('minus')
     .addEventListener('click', function()
     {
-        store.dispatch({type: 'MINUS'})
+        store.dispatch({ type: 'MINUS' })
     });
-
 document.getElementById('reset')
-.addEventListener('click', function()
+    .addEventListener('click', function()
     {
-        store.dispatch({type: 'RESET'})
+        store.dispatch({ type: 'RESET' })
     });
